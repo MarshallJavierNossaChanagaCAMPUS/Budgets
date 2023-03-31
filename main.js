870a778978d327c1d9b90c405eccd169c5369572
MarshallJavierNossaChanagaCAMPUS <infinitymc777@gmail.com>
2023-03-31 09:08:45 -0500

    build: :construction: Re Estructuracion de todo el codigo
    pendiente la maquetacion, nada es funcional an

--- main.js
+++ main.js
@@ -1,6 +1,11 @@
-let descripcion = document.querySelector("#descripcion");
+let formulario = document.querySelector("#form");
+let datos = {"mas": []};
 
-descripcion.addEventListener("submit", (e)=>{
+formulario.addEventListener("submit", (e) => {
     e.preventDefault();
-    let data = Object.fromEntries(new FormData(e.target))
-})
+    let data = Object.fromEntries(new FormData(e.target));
+    datos[`${"mas"}`];
+    formulario.reset();
+});
+
+console.log(datos);
