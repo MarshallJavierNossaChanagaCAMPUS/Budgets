90d39b0730b4b6c9871e8517e57e4858ca450fb7
MarshallJavierNossaChanagaCAMPUS <infinitymc777@gmail.com>
2023-04-09 08:39:55 -0500

    feat: :sparkles: Valor datos
    Se crea el array con los datos que se pongan en el formulario infinitamente y se imprime el ultimo valor que se digite en el formulario

--- main.js
+++ main.js
@@ -1,11 +1,38 @@
 let formulario = document.querySelector("#form");
-let datos = {"mas": []};
+let montoTotal = document.querySelector("#montoTotal");
+let datos = [];
 
-formulario.addEventListener("submit", (e) => {
+//Con este fragmento de codigo pbtengo los datos de "valor", "select" y "descripcion" y los imprimo.
+
+formulario.addEventListener("submit", (e)=>{
     e.preventDefault();
-    let data = Object.fromEntries(new FormData(e.target));
-    datos[`${"mas"}`];
+   // let data = new FormData(formulario);
+
+    let descripcion = formulario.descripcion.value;
+    let valor = Number(formulario.valor.value);
+    let select = formulario.select.value;
+    let total = valor;
+
+    if (select == 1) {
+        valor = -valor;
+        total = valor + valor
+    } else {
+        valor = valor;
+        total = valor + valor
+    };
+
+    montoTotal.innerHTML = `<h1 class="col-12 d-flex justify-content-center"> $ ${total}</h1>`;
+
+    datos.push({
+        descripcion: descripcion,
+        valor: valor,
+        select: select
+    });
+
+    console.log(datos);
+
     formulario.reset();
 });
 
-console.log(datos);
+// <h1 class="col-12 d-flex justify-content-center">$ 10.000.000.000</h1>
+
