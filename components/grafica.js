dfc8b90326de7ca2773365dcf4fb47c3a90a3f66
MarshallJavierNossaChanagaCAMPUS <infinitymc777@gmail.com>
2023-04-10 09:11:01 -0500

    feat: :sparkles: Grafica
    Añadimos la grafica, aun no es funcional

---
+++ components/grafica.js
@@ -0,0 +1,59 @@
+import config from "../storage/config.js";
+config.data();
+const datos = (this, JSON.parse(localStorage.getItem("myForm")));
+const getOptionChart = () => {
+  return {
+    
+    tooltip: {
+      trigger: "item",
+    },
+    legend: {
+      top: "5%",
+      left: "center",
+    },
+    series: [
+      {
+        name: "Access From",
+        type: "pie",
+        radius: ["40%", "70%"],
+        avoidLabelOverlap: false,
+        itemStyle: {
+          borderRadius: 10,
+          borderColor: "#fff",
+          borderWidth: 2,
+        },
+        label: {
+          show: false,
+          position: "center",
+        },
+        emphasis: {
+          label: {
+            show: true,
+            fontSize: 40,
+            fontWeight: "bold",
+          },
+        },
+        labelLine: {
+          show: false,
+        },
+        data: [
+          { value: 2 , name: "Ingresos" },
+          { value: 1 , name: "Egresos" },
+        ],
+      },
+    ],
+  };
+};
+
+const initCharts = () => {
+    const chart = echarts.init(document.querySelector("#grafica"));
+
+    chart.setOption(getOptionChart())
+}
+
+export default {
+  mostrarGrafica() {
+    window.addEventListener("load", () => {});
+    initCharts();
+  },
+};
